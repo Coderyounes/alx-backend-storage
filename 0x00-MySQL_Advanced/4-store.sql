e5695100f1e1acccd60643d8b5f404c4a2452a71
Coderyounes <younesanas2021@gmail.com>
2024-04-24 15:47:30 +0100

    Task 4 : Create Trigger to Track Quantity

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+--- SQL File Code , Create Trigger to Track Stock
+CREATE TRIGGER IF NOT EXISTS  quantityTrack 
+AFTER
+INSERT
+ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+
+END;
