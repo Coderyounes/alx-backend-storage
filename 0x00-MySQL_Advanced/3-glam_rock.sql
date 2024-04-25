f4431322a07635e3be87fdfb34044259c3ee4e1a
Coderyounes <younesanas2021@gmail.com>
2024-04-25 16:49:09 +0100

    change the Data from 2020 to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL File CODE Ranked the oldest bands
-SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
