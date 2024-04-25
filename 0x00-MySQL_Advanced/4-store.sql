8471038c7389ca6612d67e2c8113e4a16549ee8b
Coderyounes <younesanas2021@gmail.com>
2024-04-25 16:57:07 +0100

    try this one

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,12 @@
 --- SQL File Code , Create Trigger to Track Stock
-DELIMITER //
-
-CREATE TRIGGER decrease_quantity_after_order
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END//
-
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
 DELIMITER ;
