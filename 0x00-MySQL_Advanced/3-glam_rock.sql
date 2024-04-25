0f8f2e237ad644ad2c9308d5fe44f28658ab1088
Coderyounes <younesanas2021@gmail.com>
2024-04-25 16:47:41 +0100

    switch back to mine

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL File CODE Ranked the oldest bands
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
