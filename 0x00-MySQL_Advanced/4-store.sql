c228a4bb7a14009af12f8a1fbbc2216aa15116fa
Coderyounes <younesanas2021@gmail.com>
2024-04-25 21:25:32 +0100

    update the  Task "buybuybuy"

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,5 @@
 --- SQL File Code , Create Trigger to Track Stock
 CREATE TRIGGER decrement
-AFTER INSERT
-ON orders
+AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
