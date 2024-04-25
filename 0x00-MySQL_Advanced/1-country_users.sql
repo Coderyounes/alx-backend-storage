fa7f3fb7809364eec7ed147e6261f3afc3d7bac7
Coderyounes <younesanas2021@gmail.com>
2024-04-25 16:42:28 +0100

    next Try for Task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@ Create Table users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255)
-    country NOT NULL ENUM ('US', 'CO', 'TN') DEFAULT 1,
+    country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US',
     PRIMARY KEY(id)
 );
