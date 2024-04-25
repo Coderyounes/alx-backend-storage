0d0b64a52b9372b377ee91cbe21cc07061ac09ac
Coderyounes <younesanas2021@gmail.com>
2024-04-25 16:43:12 +0100

    Just ask my friend

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- SQL File Code Create Table user
 DROP TABLE IF EXISTS users;
-Create Table users (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255)
-    country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US',
-    PRIMARY KEY(id)
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
