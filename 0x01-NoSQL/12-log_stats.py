cd151baaf88c5aa05d15e18584a2c99ce10f7e0d
Coderyounes <younesanas2021@gmail.com>
2024-04-30 16:23:42 +0100

    Task 12 : Parse Logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Documentation """
+from pymongo import MongoClient
+
+
+def print_nginx_request_logs(nginx_collection):
+    """
+    Prints the statistics of nginx request logs.
+
+    Args:
+        nginx_collection: The collection of nginx request logs.
+
+    Returns:
+        None
+    """
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+nginx_collection = client.logs.nginx
+
+print_nginx_request_logs(nginx_collection)
