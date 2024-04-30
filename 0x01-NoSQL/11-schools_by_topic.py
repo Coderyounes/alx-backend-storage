5a50598756fc5f5766366c2582a8533be8ade6ce
Coderyounes <younesanas2021@gmail.com>
2024-04-30 15:55:57 +0100

    task 11 : pycodestyle fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 """ Documentation """
 
+
 def schools_by_topic(mongo_collection, topic):
+    """
+    Retrieve schools from a MongoDB collection based on a given topic.
+
+    Args:
+        mongo_collection: The MongoDB collection to query.
+        topic (str): The topic to filter schools by.
+
+    Returns:
+        A cursor object containing the schools matching the given topic.
+    """
     return mongo_collection.find({"topics": topic})
